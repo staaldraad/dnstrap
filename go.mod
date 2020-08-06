660c034695662f292e177b3236b5d2ebb4c62edd
Etienne Stalmans <estalmans@gmail.com>
2020-08-06 15:40:15 +0200

    basic

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/staaldraad/dnstrap
+
+require github.com/miekg/dns v1.1.31
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+        "flag"
+        "fmt"
+        "github.com/miekg/dns"
+        "os"
+        "strconv"
+        "syscall"
+        "os/signal"
+)
+
+func handleReflect(w dns.ResponseWriter, r *dns.Msg) {
+        m := new(dns.Msg)
+        m.SetReply(r)
+        m.Compress = false
+
+        switch r.Opcode {
+        case dns.OpcodeQuery:
+                parseQuery(m)
+        }
+
+        w.WriteMsg(m)
+}
+
+func parseQuery(m *dns.Msg) {
+        for _, q := range m.Question {
+                switch q.Qtype {
+                case dns.TypeA:
+                        fmt.Printf("Query for %s\n", q.Name)
+                        ip := "1.1.1.1" 
+                        if ip != "" {
+                                rr, err := dns.NewRR(fmt.Sprintf("%s A %s", q.Name, ip))
+                                if err == nil {
+                                        m.Answer = append(m.Answer, rr)
+                                }
+                        }
+                }
+        }
+}
+
+func main() {
+        rootDomain := flag.String("domain", "", "root domain to use (example: mydomain.pw.)")
+        flag.Parse()
+
+        if *rootDomain == "" {
+                fmt.Println("-domain required")
+                os.Exit(1)
+        }
+
+        dns.HandleFunc(*rootDomain, handleReflect)
+
+        port := 53
+        server := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "udp"}
+        fmt.Printf("Starting at %d\n", port)
+        go func() {
+                err := server.ListenAndServe()
+                defer server.Shutdown()
+                if err != nil {
+                        fmt.Printf("Failed to start server: %s\n ", err.Error())
+                        os.Exit(1)
+                }
+        }()
+        sig := make(chan os.Signal)
+        signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+        s := <-sig
+        fmt.Printf("Signal (%s) received, stopping\n", s)
+}
+
+
